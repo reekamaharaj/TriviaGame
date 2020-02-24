72e03ea88ec46bfa7599d7bfb6865bc01177e612
Reeka <reekamaharaj@gmail.com>
2020-02-23 22:08:51 -0600

    js file updated with first thought notes and how I want to make this work

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+/* 
+TODO Create start button 
+TODO When the trivia starts there is a timer. (about 120 seconds might be good)
+TODO Questions will populate in a form layout
+TODO Only one answer should be selectable
+TODO Button to say that you are complete, if the time hasn't run out
+TODO When the timer runs out the player should be taken to the end page
+TODO Text saying they didn't finish on time
+TODO Text different if they did finish on time, or used the Done button
+TODO End screen will have the number of correct answers, number of wrong answers, number of unanswered questions and a retry option. 
+TODO Consider making the question randomized so that if they try multiple times there are different questions
+TODO Counter should be displayed so the player knows how much time they have left. 
+*/
+
+// Buttons
+// Start
+// Done
+// Restart
+
+//----Load page
+//Directions for the game
+//Start button
+//Extra: different topic options
+
+//-----Trivia page stuuf
+//Make a form layout with bubble options for answers
+//Timer area
+
+//----Result page
+//Restart buttom
+//# Correctly Answered Questions
+//# Incorrectly Answered Questions
+//# Unanswered Questions
+//Different text for if the player ran out of time or if they hit the Done button
+
+//----JS stuff
+//Game start function - reset/sets the timer when the Start or restart button is clicked
+//Randomize the questions
+//Generate the questions to the page
+//Check the final answers with the correct ones. Only do this if the player hits Done or if the timer runs out
+//function for what happens when the done button is used
+//function for what happens when the timer runs out
+//function to update the timer on the page
+
