a4f77b5031bc49c32872b1f57bd6aa73eb469d5e
Reeka <reekamaharaj@gmail.com>
2020-02-28 21:04:46 -0600

    game mechanics finished, no bugs that I have noticed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,9 +9,7 @@ const triviaQuestion = [
             c: 230, 
             d: 186
         },
-        correct: function(){
-                return this.answer.c;
-            } 
+        correct: "c"
     },
 
     {
@@ -22,9 +20,7 @@ const triviaQuestion = [
             c: "Siamese", 
             d: "Persian"
         },
-        correct: function(){
-            return this.answer.c;
-        }
+        correct: "c"
     },
 
 
@@ -35,9 +31,7 @@ const triviaQuestion = [
             b: 7,
             c: 2,
             d: 6},
-        correct: function(){
-            return this.answer.a;
-        }
+        correct: "a"
     },
 
     {
@@ -47,9 +41,7 @@ const triviaQuestion = [
             b: 30,
             c: 36,
             d: 22},
-        correct: function(){
-            return this.answer.b;
-        }
+        correct: "b"
     },
 
     {
@@ -59,130 +51,108 @@ const triviaQuestion = [
             b: "Belgian Mist",
             c: "Australian Mist",
             d: "American Mist"},
-        correct: function(){
-            return this.answer.c;
-        }
+        correct: "c"
     },
 
-    // {
-    //     question: "Which of these cat breeds has long hair?",
-    //     answer:{
-    //         a: "Ocicat",
-    //         b: "Havana Brown",
-    //         c: "Russian blue",
-    //         d: "Himalayan"},
-    //     correct: function(){
-    //         return this.answer.d;
-    //     }
-    // },
+    {
+        question: "Which of these cat breeds has long hair?",
+        answer:{
+            a: "Ocicat",
+            b: "Havana Brown",
+            c: "Russian blue",
+            d: "Himalayan"},
+        correct: "d"
+    },
     
-    // {
-    //     question: "Which of these breeds is caused by a genetic mutation?",
-    //     answer: {
-    //         a: "Siberian",
-    //         b: "Manx",
-    //         c: "Munchkin",
-    //         d: "Napoleon"},
-    //     correct: function(){
-    //         return this.answer.c;
-    //     }
-    // },
+    {
+        question: "Which of these breeds is caused by a genetic mutation?",
+        answer: {
+            a: "Siberian",
+            b: "Manx",
+            c: "Munchkin",
+            d: "Napoleon"},
+        correct: "c"
+    },
 
-    // {
-    //     question: "Which of these breeds is native to Kenya?",
-    //     answer: {
-    //         a: "Sokoke",
-    //         b: "Nebelung",
-    //         c: "Tiffany",
-    //         d: "Snowshoe"},
-    //     correct: function(){
-    //         return this.answer.a;
-    //     }
-    // },
+    {
+        question: "Which of these breeds is native to Kenya?",
+        answer: {
+            a: "Sokoke",
+            b: "Nebelung",
+            c: "Tiffany",
+            d: "Snowshoe"},
+        correct: "a"
+    },
 
-    // {
-    //     question: "Which of these cats has a 'ticked coat'?",
-    //     answer: {
-    //         a: "Manx",
-    //         b: "La Perm",
-    //         c: "Persian",
-    //         d: "Abyssinian"},
-    //     correct: function(){
-    //         return this.answer.d;
-    //     }
-    // },
+    {
+        question: "Which of these cats has a 'ticked coat'?",
+        answer: {
+            a: "Manx",
+            b: "La Perm",
+            c: "Persian",
+            d: "Abyssinian"},
+        correct: "d"
+    },
 
-    // {
-    //     question: "Which breed of cat is the largest?",
-    //     answer: {
-    //         a: "Savannah",
-    //         b: "Maine Coon",
-    //         c: "Ragdoll",
-    //         d: "Siberian"},
-    //     correct: function(){
-    //         return this.answer.b;
-    //     }
-    // },
+    {
+        question: "Which breed of cat is the largest?",
+        answer: {
+            a: "Savannah",
+            b: "Maine Coon",
+            c: "Ragdoll",
+            d: "Siberian"},
+        correct: "b"
+    },
     
-    // {
-    //     question: "When does a kitten open its eyes?",
-    //     answer: {
-    //         a: "5 weeks",
-    //         b: "Two months",
-    //         c: "1-3 weeks",
-    //         d: "At birth"},
-    //     correct: function (){
-    //         return this.answer.c;
-    //     }
-    // },
+    {
+        question: "When does a kitten open its eyes?",
+        answer: {
+            a: "5 weeks",
+            b: "Two months",
+            c: "1-3 weeks",
+            d: "At birth"},
+        correct: "c"
+    },
     
-    // {
-    //     question: "When does a kitten show interest in solid food?",
-    //     answer: {
-    //         a: "Two months",
-    //         b: "At birth",
-    //         c: "2 weeks",
-    //         d: "6 weeks"},
-    //     correct: function(){
-    //         return this.answer.d;
-    //     }
-    // },
+    {
+        question: "When does a kitten show interest in solid food?",
+        answer: {
+            a: "Two months",
+            b: "At birth",
+            c: "2 weeks",
+            d: "6 weeks"},
+        correct: "d"
+    },
 
-    // {
-    //     question: "What best describes a feral cat?",
-    //     answer:{
-    //         a: "A domesticated cat that has returned to the wild",
-    //         b: "A barn cat",
-    //         c: "Cats that roam only at night",
-    //         d: "Cats that roam freely"},
-    //     correct: function(){
-    //         return this.answer.a;
-    //     }
-    // },
+    {
+        question: "What best describes a feral cat?",
+        answer:{
+            a: "A domesticated cat that has returned to the wild",
+            b: "A barn cat",
+            c: "Cats that roam only at night",
+            d: "Cats that roam freely"},
+        correct: "a"
+    },
 
-    // {
-    //     question: "What is the name of the cat that was left £9m by his reclusive British owner in 1988?",
-    //     answer: {
-    //         a: "Blackie",
-    //         b: "Paws",
-    //         c: "Tubbs",
-    //         d: "Marmalade"},
-    //     correct: function(){
-    //         return this.answer.a;
-    //     }
-    // },
+    {
+        question: "What is the name of the cat that was left £9m by his reclusive British owner in 1988?",
+        answer: {
+            a: "Blackie",
+            b: "Paws",
+            c: "Tubbs",
+            d: "Marmalade"},
+        correct: "a"
+    },
 
-    // {
-    //     question: "On average, cats spend how much time every day sleeping?",
-    //     answer: {
-    //         a: "8 Hours",
-    //         b: "12 Hours",
-    //         c: "4 Hours",
-    //         d: "16 Hours"},
-    //     correct: function(){
-    //         return this.answer.d;
-    //     }
-    // }
+    {
+        question: "On average, cats spend how much time every day sleeping?",
+        answer: {
+            a: "8 Hours",
+            b: "12 Hours",
+            c: "4 Hours",
+            d: "16 Hours"},
+        correct: "d"
+    }
 ];
 
 let questionContainer;
@@ -196,9 +166,10 @@ let correct = 0;
 let incorrect =0;
 let unanswered = 0;
 let answers = {};
+var randArr = [ ];
 
 let timerRunning = false;
-const time = 300;
+let time = 20;
 
 //document load
 $('document').ready(function(){
@@ -220,18 +191,34 @@ $('document').ready(function(){
 //start 
 function start(){
     timerStart();
-    populateTriv();
+    randQuestions();
+
     $("#timer").show();
     $("#done").show();
     $("#triviaAll").show();
     $("#start").hide();
     $("#howto").hide();
+
+    console.log("correct " + correct + "incorrect " + incorrect + "unanswered " + unanswered);
 }
+
+//Random Questions
+function randQuestions(){
+    for (i=0; i < 5; i++){
+        var random = triviaQuestion[Math.floor(Math.random()*15)];
+        randArr.push(random);
+    }    
+    console.log(randArr);
+    populateTriv();
+    console.log("correct " + correct + "incorrect " + incorrect + "unanswered " + unanswered);
+}
+
 
 //questions added
 function populateTriv(){
-    for (i=0; i < triviaQuestion.length; i++){
+    for (i=0; i < randArr.length; i++){
 
+        unanswered++;
         questionContainer = undefined;
         question = undefined;
         answerA = undefined;
@@ -241,15 +228,15 @@ function populateTriv(){
 
         questionContainer = $('<div class="flex mt-2" id="questionContainer">');
 
-        question = '<label class="inline-flex items-center px-4 py-2 font-bold text-orange-800" id="question">' + '<span class="ml-2 px-4 py-2 text-orange-800">' + triviaQuestion[i].question + '</span>' + '</label>';
+        question = '<label class="inline-flex items-center px-4 py-2 font-bold text-orange-800" id="question">' + '<span class="ml-2 px-4 py-2 text-orange-800">' + randArr[i].question + '</span>' + '</label>';
 
-        answerA = '<label class="inline-flex items-center px-4 py-2 font-bold text-orange-800" id="answerA">' + '<input type="radio" class="' + i + '" name="answer' + i + '" value="a" onClick="clicked(this.value, this.className)">' + '</input>' + '<span class="ml-2 px-4 py-2 text-orange-800">' + triviaQuestion[i].answer.a + '</span>' + '</label>';
+        answerA = '<label class="inline-flex items-center px-4 py-2 font-bold text-orange-800" id="answerA">' + '<input type="radio" class="' + i + '" name="answer' + i + '" value="a" onClick="clicked(this.value, this.className)">' + '</input>' + '<span class="ml-2 px-4 py-2 text-orange-800">' + randArr[i].answer.a + '</span>' + '</label>';
 
-        answerB = '<label class="inline-flex items-center px-4 py-2 font-bold text-orange-800" id="answerB">' + '<input type="radio" class="' + i + '" name="answer' + i + '" value="b" onClick="clicked(this.value, this.className)">' + '</input>' + '<span class="ml-2 px-4 py-2 text-orange-800 ">' + triviaQuestion[i].answer.b + '</span>' + '</label>';
+        answerB = '<label class="inline-flex items-center px-4 py-2 font-bold text-orange-800" id="answerB">' + '<input type="radio" class="' + i + '" name="answer' + i + '" value="b" onClick="clicked(this.value, this.className)">' + '</input>' + '<span class="ml-2 px-4 py-2 text-orange-800 ">' + randArr[i].answer.b + '</span>' + '</label>';
 
-        answerC = '<label class="inline-flex items-center px-4 py-2 font-bold text-orange-800"id="answerC">' + '<input type="radio" class="' + i + '" name="answer' + i + '" value="c" onClick="clicked(this.value, this.className)">' + '</input>' + '<span class="ml-2 px-4 py-2 text-orange-800 ">' + triviaQuestion[i].answer.c + '</span>' + '</label>';
+        answerC = '<label class="inline-flex items-center px-4 py-2 font-bold text-orange-800"id="answerC">' + '<input type="radio" class="' + i + '" name="answer' + i + '" value="c" onClick="clicked(this.value, this.className)">' + '</input>' + '<span class="ml-2 px-4 py-2 text-orange-800 ">' + randArr[i].answer.c + '</span>' + '</label>';
 
-        answerD = '<label class="inline-flex items-center px-4 py-2 font-bold text-orange-800" id="answerD">' + '<input type="radio" class="' + i + '" name="answer' + i + '" value="d" onClick="clicked(this.value, this.className)">' + '</input>' + '<span class="ml-2 px-4 py-2 text-orange-800 ">' + triviaQuestion[i].answer.d + '</span>' + '</label>';
+        answerD = '<label class="inline-flex items-center px-4 py-2 font-bold text-orange-800" id="answerD">' + '<input type="radio" class="' + i + '" name="answer' + i + '" value="d" onClick="clicked(this.value, this.className)">' + '</input>' + '<span class="ml-2 px-4 py-2 text-orange-800 ">' + randArr[i].answer.d + '</span>' + '</label>';
 
         questionContainer.append(question, answerA, answerB, answerC, answerD);
         $("#triviaAll").append(questionContainer);
@@ -286,42 +273,47 @@ function endTrivia(){
 
     $("#correctAns").show().text("Correct: " + correct);
     $("#incorrectAns").show().text("Incorrect: " + incorrect);
-    unanswered = triviaQuestion.length - (correct + incorrect);
+    unanswered = randArr.length - (correct + incorrect);
     $("#unanswered").show().text("Unanswered: " + unanswered);
 
     results();
     clearInterval(intervalId);
     timerRunning = false;
     time = 20;
+    
+    console.log("correct " + correct + "incorrect " + incorrect + "unanswered " + unanswered);
 }
 
 function clicked(currentAns, qIndex){
     const qIndexInt = parseInt(qIndex);
-    const question = triviaQuestion[qIndexInt];
-    
+    const question = randArr[qIndexInt];
+
     const currentAnsObj = {
-        correctAns: question.correct(),
+        correctAns: question.correct,
         radioAns: currentAns
     }
 
     answers[qIndexInt] = currentAnsObj;
-}
+    answerArray = Object.values(answers);
+
+    console.log("correct " + correct + "incorrect " + incorrect + "unanswered " + unanswered);
+    }
+
 
 // results
 function results(){
-    
-    console.log("correct  " + correct);
-    console.log("incorrect  " + incorrect);
-    console.log("unanswered  " + unanswered);
-    
-    if (){
-        correct++;
-        unanswered--;
+    for (i=0; i < answerArray.length; i++){
+        if (answerArray[i].correctAns === answerArray[i].radioAns){
+            correct ++;
+            unanswered--;
+        }
+        else {
+            incorrect ++;
+            unanswered--;
+        }
     }
-    else {
-        incorrect++;
-        unanswered--;
-    }
+
+    console.log("correct " + correct + "incorrect " + incorrect + "unanswered " + unanswered);
 
     $("#correctAns").text("Correct Answers: " + correct);
     $("#incorrectAns").text("Incorrect Answers: " + incorrect);
@@ -349,6 +341,8 @@ function reset(){
     timerRunning = false;
     time = 20;
     unanswered = 0;
+    answers = { };
+    randArr = [ ];
 
     start();
 }
